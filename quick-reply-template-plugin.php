afadb33c44e681aaceda49a6149495e124292d4b
paulwilliam <paul@entropytheblog.com>
2008-12-04 20:49:44 +1300

    first commit

---
+++ quick-reply-template-plugin.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Quick Reply Template Plugin
+Plugin URI: http://www.entropytheblog.com/
+Description: Allows you to specify a reply template for the quick reply feature in Wordpress 2.7+. The template can contain the comment author's fullname and firstname and any other characters.
+Version: 0.1
+Author: Paul William
+Author URI: http://www.entropytheblog.com/blog/
+
+Copyright 2008 Paul William
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Quick Reply Template Plugin ===
+Contributors: paulwilliam
+Requires at least: 2.7
+Tested up to: 2.7
+Stable tag: trunk
+Tags: reply, template, quick, comments, author
+
+Allows you to specify a reply template for the quick reply feature in Wordpress 2.7+.
+
+== Description ==
+
+Allows you to specify a reply template for the quick reply feature in Wordpress 2.7+. The template can contain the comment author's fullname and firstname and any other characters.
+
+== Installation ==
+
+1) Download the plugin zip file.
+
+2) Unzip.
+
+3) Upload the quick-reply-template directory to your wordpress plugin directory (/wp-content/plugins).
+
+4) Activate the plugin.
+
+5) Open up the options and set your desired template.
+
+6) When you click reply in the admin comments page, your reply template will be automatically inserted.
